ce8913a9f1bbc8a9d92ca7bbc3d88556a4f61efb
satyam0507 <satyam0507@gmail.com>
2017-03-08 19:00:34 +0530

    offline functionality added

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
-/*
-  Copyright 2014 Google Inc. All Rights Reserved.
+//    Copyright 2017 Tagnpin Web Solutions
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
 
-      http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
 'use strict';
 
 var browserify = require('browserify');
@@ -54,7 +54,7 @@ gulp.task('build', function() {
     .pipe(source('sw-toolbox.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(uglify())
+    .pipe(uglify({compress:false}))
     .pipe(header(license))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./'));
