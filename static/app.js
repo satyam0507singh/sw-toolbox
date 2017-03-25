566bf8296e856172c9c593f70ff2aed3aa007c1e
satyam0507@gmail.com <satyam0507@gmail.com>
2017-03-25 16:50:55 +0500

    adding offline css change (in process)

--- static/app.js
+++ static/app.js
@@ -14,11 +14,96 @@
 
 console.log('app.js');
 
-if(navigator.serviceWorker){
-    navigator.serviceWorker.register('sw.js',{scope:'/'}).then(function(){
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('sw.js', { scope: '/' }).then(function () {
         console.log('service worker registered successfully');
-    }).catch(function(err){
-        console.log('not able to register service worker :: '+err);
+    }).catch(function (err) {
+        console.log('not able to register service worker :: ' + err);
     })
 }
 
+window.addEventListener('offline',getList);
+window.addEventListener('online',reload);
+
+function reload(){
+    location.reload();
+}
+
+if (!navigator.onLine) {
+    getList();
+}
+
+function getList() {
+    caches.open('sw-test').then(function (cache) {
+        cache.keys().then(function (keys) {
+            modifyLinks(keys);
+        })
+    }).catch(function (err) {
+        console.log(err);
+    })
+
+}
+
+function modifyLinks(links) {
+    var _urls = [];
+    var _origin = location.origin;
+    for (var key of links) {
+        _urls.push(new URL(key.url));
+    }
+    var urls = _urls.map(function (key) {
+        if (key.origin === _origin) {
+            return key;
+        }
+    })
+    getAllKeys(urls)
+}
+
+function getAllKeys(_urls) {
+    var elType = ['a'];
+    var elem = [];
+    for (var elStr of elType) {
+        for (var key of _urls) {
+            var searchStr = 'a[href="' + key.pathname + '"]';
+            var el = document.querySelectorAll(searchStr);
+            if (el) {
+                elem.push(el);
+            }
+        }
+    }
+    addCss(elem);
+}
+
+function addCss(elemArray) {
+    var validElArray = elemArray.map(function (elNode) {
+        if (elNode.length > 0) {
+            return elNode;
+        } else {
+            return;
+        }
+    })
+    for (var elArray of validElArray) {
+        if (elArray) {
+            for (el of elArray) {
+                if (el) {
+                    el.classList.add('available');
+                }
+            }
+
+        }
+    }
+    addScript();
+}
+function addScript() {
+    var style = document.createElement("style");
+    style.textContent = `a:not(.available){
+                            color: #ddd!important;
+                        }
+                        a:not(.available):hover{
+                            color: #ddd!important;
+                        }
+                        a:not(.available):active{
+                            color: #ddd!important;
+                        }`;
+    document.body.appendChild(style);
+
+}
